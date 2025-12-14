2eb306f498ee3cb619367e6cd33ebd8e320dd129
John Witulski <jowit001@hhu.de>
2025-12-15 00:42:06 +0100

    Add PacMan draft

---
+++ 65c02Assembly/PacMan/ReadMe.md
@@ -0,0 +1,29 @@
+# PacMan
+
+A Cody Assembly Game for the [Cody Computer](https://www.codycomputer.org/).
+
+# How to Play
+
+None
+
+# Screenshot
+![pacman.png](pacman.png)
+
+# Author
+
+John Witulski
+
+# Run (Emulation)
+Run using  [Cody Computer Emulator](https://github.com/iTitus/cody_emulator):
+`cargo run --release -- --as-cartridge pacman.bin`
+
+# Run (Real Hardware)
+
+Run the program on the Cody computer using the Prop Plug. 
+
+`LOAD 1,1` 
+
+Or put the game on a cartridge.
+
+# TODO:
+- everything
---
+++ 65c02Assembly/PacMan/pacman.asm
@@ -0,0 +1,339 @@
+; PacMan
+
+.include "codyconstants.asm"
+
+; Program header for Cody Basic's loader (needs to be first)
+
+.WORD ADDR                      ; Starting address (just like KIM-1, Commodore, etc.)
+.WORD (ADDR + LAST - MAIN - 1)  ; Ending address (so we know when we're done loading)
+
+; The actual program.
+
+.LOGICAL    ADDR                ; The actual program gets loaded at ADDR
+
+MAIN                            ; The program starts running from here
+            LDA #$E2            ; Set border color (Bits 0-3) to red=2 
+                                ; and set color memory to $D800 (A000+14*1024=D800), E=14
+            STA VID_COLR        ; VID_COLR=$D002 (see codyconstants.asm)
+            LDA #$95            ; Set character memory to $C800 (A000+5*2048=C800)
+                                ; and set screen memory location $C400 (A000+9*1024=C400)
+            STA VID_BPTR        ; VID_BPTR=$D003 (see codyconstants.asm)
+
+            LDA #$E7            ; Store shared colors (light blue=14 and yellow=7)
+            STA VID_SCRC        ; VID_SCRC=$D005 (see codyconstants.asm) 
+
+            LDX #0              ; Copy character
+_COPYCHAR   LDA TILE_DATA,X
+            STA $C800,X
+            INX
+            CPX #(8*14)          ; every tile is 8 Bytes, 14 tiles
+            BNE _COPYCHAR
+ 
+            LDY #0               ; every loop copies 240 tiles
+_ROWS
+            LDA MAP_DATA,Y      
+            STA $C400, Y
+            INY
+            CPY #240
+            BNE _ROWS
+
+            LDY #0
+_ROWS2
+            LDA MAP_DATA2,Y      
+            STA $C4F0, Y    ; + 240 
+            INY
+            CPY #240
+            BNE _ROWS2
+
+            LDY #0
+_ROWS3
+            LDA MAP_DATA3,Y      
+            STA $C5E0, Y    ; + 240*2
+            INY
+            CPY #240
+            BNE _ROWS3
+
+            LDY #0
+_ROWS4
+            LDA MAP_DATA4,Y      
+            STA $C6D0, Y    ; + 240*3
+            INY
+            CPY #240
+            BNE _ROWS4
+
+            LDY #0
+_ROWS5
+            LDA MAP_DATA5,Y      
+            STA $C7C0, Y    ; + 240*4
+            INY
+            CPY #40
+            BNE _ROWS5
+
+            LDX #0              ; Copy sprite data into video memory
+_COPYSPRT   LDA SPRITEDATA,X
+            STA $A400,X         ; sprite pixel data location. Page 327 and 535 
+            INX
+            CPX #(64*2)         ; copy data for 2 sprites 
+            BNE _COPYSPRT
+            
+            LDA #$00            ; Sprite bank 0, black as common sprite color 
+            STA VID_SPRC        ; VID_SPRC=$D006 (see codyconstants.asm)
+
+            LDA #$10            ; ($A400-$A000)/$40=$10 see Page 327 for explaination
+            STA SPR0_PTR        ; SPR0_PTR=$D083 (see codyconstants.asm)
+            LDA #$12            ; red=2 color 1, white=1 color 2 
+            STA SPR0_COL        ; SPR0_COL=$D082 (see codyconstants.asm)
+            LDA #80    ; set initial left bat sprite X position
+            STA SPR0_X       
+            LDA #(21+8)
+            STA SPR0_Y
+
+            LDA #$10            ; ($A400-$A000)/$40=$10 see Page 327 for explaination
+            STA SPR0_PTR+4      ; SPR0_PTR=$D083 (see codyconstants.asm)
+            LDA #$1A            ; A color 1, white=1 color 2 
+            STA SPR0_COL+4      ; SPR0_COL=$D082 (see codyconstants.asm)
+            LDA #(80-8)         ; set initial left bat sprite X position
+            STA SPR0_X+4       
+            LDA #(21+32)
+            STA SPR0_Y+4
+
+            LDA #$10            ; ($A400-$A000)/$40=$10 see Page 327 for explaination
+            STA SPR0_PTR+8      ; SPR0_PTR=$D083 (see codyconstants.asm)
+            LDA #$1E            ; E color 1, white=1 color 2 
+            STA SPR0_COL+8      ; SPR0_COL=$D082 (see codyconstants.asm)
+            LDA #(16)           ; set initial left bat sprite X position
+            STA SPR0_X+8       
+            LDA #(21+32)
+            STA SPR0_Y+8
+
+            LDA #$10            ; ($A400-$A000)/$40=$10 see Page 327 for explaination
+            STA SPR0_PTR+12     ; SPR0_PTR=$D083 (see codyconstants.asm)
+            LDA #$1D            ; D color 1, white=1 color 2 
+            STA SPR0_COL+12     ; SPR0_COL=$D082 (see codyconstants.asm)
+            LDA #(32)           ; set initial left bat sprite X position
+            STA SPR0_X+12       
+            LDA #(21+40)
+            STA SPR0_Y+12
+
+            LDA #$11            ; ($A400-$A000)/$40=$10 see Page 327 for explaination
+            STA SPR0_PTR+16     ; SPR0_PTR=$D083 (see codyconstants.asm)
+            LDA #$07            ; yellow=7 color 1, black=1 color 2 (not used in sprite)
+            STA SPR0_COL+16     ; SPR0_COL=$D082 (see codyconstants.asm)
+            LDA #(12+64)           ; set initial left bat sprite X position
+            STA SPR0_X+16       
+            LDA #(21+88)
+            STA SPR0_Y+16
+
+_DONE       JMP _DONE           ; Loops forever
+
+; DATA SECTION
+SPRITEDATA
+
+.BYTE %00_00_01_01, %01_01_00_00, %00_00_00_00 ; ghost
+.BYTE %00_01_01_01, %01_01_01_00, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_10_10_01, %01_10_10_01, %00_00_00_00
+.BYTE %01_11_10_01, %01_11_10_01, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_01_00_01, %01_00_01_01, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00
+
+.BYTE %00_00_01_01, %01_01_00_00, %00_00_00_00 ; pacman
+.BYTE %00_01_01_01, %01_01_01_00, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %00_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %00_00_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %00_00_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %00_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %01_01_01_01, %01_01_01_01, %00_00_00_00
+.BYTE %00_01_01_01, %01_01_01_00, %00_00_00_00
+.BYTE %00_00_01_01, %01_01_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00, %00_00_00_00, %00_00_00_00
+.BYTE %00_00_00_00
+
+TILE_DATA
+
+  .BYTE %00000000   ; "empty tile"
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00000000   ; "small pill"
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00100000
+  .BYTE %00100000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00000000   ; "big pill"
+  .BYTE %00000000
+  .BYTE %00101000
+  .BYTE %00101000
+  .BYTE %00101000
+  .BYTE %00101000
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00000000   
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %11000000
+  .BYTE %11000000
+  .BYTE %11001111
+  .BYTE %11001100
+  .BYTE %11001100
+
+  .BYTE %00000000   
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00000000   
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000011
+  .BYTE %00000011
+  .BYTE %11110011
+  .BYTE %00110011
+  .BYTE %00110011
+
+  .BYTE %11001100   
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001100
+
+  .BYTE %11000011  
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+
+  .BYTE %00110011  
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %00110011
+
+  .BYTE %00000000   
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+
+  .BYTE %11001100   
+  .BYTE %11001100
+  .BYTE %11001100
+  .BYTE %11001111
+  .BYTE %11000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00000000  
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %00110011   
+  .BYTE %00110011
+  .BYTE %00110011
+  .BYTE %11110011
+  .BYTE %00000011
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+
+  .BYTE %11000011  
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %11000011
+  .BYTE %00000000
+  .BYTE %11111111
+  .BYTE %00000000
+  .BYTE %00000000
+
+MAP_DATA
+  .BYTE 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1, 3, 4, 5, 1, 3, 4, 5, 1, 7, 0, 3, 4, 5, 0, 3, 4, 5, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 2, 6, 0, 8, 1, 6, 0, 8, 1, 7, 0, 6, 0, 8, 0, 6, 0, 8, 2, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1,10,11,12, 1,10,11,12, 1, 7, 0,10,11,12, 0,10,11,12, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+MAP_DATA2
+  .Byte 6, 1, 3, 4, 5, 1, 6, 1, 3, 4, 4, 4, 5, 0, 6, 0, 3, 4, 5, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1,10,11,12, 1, 6, 1,10,11, 9,11,12, 0, 6, 0,10,11,12, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 1, 1, 1, 1, 1, 6, 1, 1, 1, 7, 0, 0, 0, 6, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 10,4, 4, 5, 0, 0, 6, 4, 4, 1, 7, 0, 4, 4, 6, 0, 0, 0, 4, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 4, 4, 4, 12,0, 0, 6, 1, 1, 1, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 0, 0, 0, 0, 0, 0, 0, 1, 3, 4, 4, 4, 5, 1, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+MAP_DATA3
+  .Byte 0, 0, 0, 0, 0, 0, 0, 1, 6, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 4, 4, 4, 5, 0, 0, 6, 1, 6, 0, 0, 0, 8, 1, 6, 0, 0, 3, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 3, 4, 4,12, 0, 0, 6, 1,10,11,11,11,12, 1, 6, 0, 0,10, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 0, 4, 5, 0, 0, 4, 4, 4, 4, 0, 4, 4, 4, 4, 0, 0, 4, 5, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 2, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 2, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+MAP_DATA4
+  .Byte 10,5, 0, 8, 0, 0, 6, 0, 3, 4, 4, 4, 5, 0, 6, 0, 0, 8, 0, 3,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 3,12, 0, 8, 0, 0, 6, 0,10,11,11,11,12, 0, 6, 0, 0, 8, 0,10, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 0, 0, 0, 0, 0, 6, 0, 0, 0, 6, 0, 0, 0, 6, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 0, 4, 4, 4, 4, 4, 4, 4, 0, 6, 0, 4, 4, 4, 4, 4, 4, 4, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  .Byte 10,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,12,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+MAP_DATA5
+  .Byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+LAST                            ; End of the entire program
+
+.ENDLOGICAL
