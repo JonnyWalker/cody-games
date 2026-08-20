6aad7b90518bc210de1f6342bb7a85ccdee448a4
John Witulski <jowit001@hhu.de>
2026-08-20 11:51:45 +0200

    port changes from 6 to 9 (house and tree tile)

--- 65c02Assembly/DemoCode/09_level_with_sprite.asm
+++ 65c02Assembly/DemoCode/09_level_with_sprite.asm
@@ -56,7 +56,7 @@ _EMPTY3
 _COPYCHAR   LDA CHARDATA,X
             STA $C800,X
             INX
-            CPX #8              ; copy 8 Bytes
+            CPX #144          ; copy 18*8 Bytes
             BNE _COPYCHAR
     
             LDX #0              ; Copy sprite data into video memory
@@ -119,6 +119,20 @@ _COPYCOLOR3 LDA #$05            ; forground color (0=black) backgrund color (5=g
             CPX #20
             BNE _XLOOP
 
+            LDA #$7F            ; Store shared colors (yellow=7 and light gray=15)
+            STA VID_SCRC        ; VID_SCRC=$D005 (see codyconstants.asm)
+
+            LDA #1              ; house 0
+            STA $C569
+            LDA #2              ; house 1
+            STA $C56A
+            LDA #3              ; house 2
+            STA $C56B
+            LDA #4              ; house 3
+            STA $C56C
+            LDA #5              ; house 4
+            STA $C56D
+
             LDX #0              ; Change tile color 
 _XLOOP2
             LDY #0              
@@ -140,29 +154,44 @@ _COPYCOLOR4 LDA #$0C            ; forground color (0=black) backgrund color (5=g
             CPX #5
             BNE _XLOOP2 
 
-            LDA #$72            ; Store shared colors (light blue=14 and red=2)
-            STA VID_SCRC        ; VID_SCRC=$D005 (see codyconstants.asm)
-
-            LDA #1              ; print C
+            LDA #6              ; print C
             STA $C7D0
-            LDA #2              ; print o
+            LDA #7              ; print o
             STA $C7D1
-            LDA #3              ; print d
+            LDA #8              ; print d
             STA $C7D2
-            LDA #4              ; print y
+            LDA #9              ; print y
             STA $C7D3
-            LDA #5              ; print v
+            LDA #10             ; print v
             STA $C7D4
-            LDA #6              ; print i
+            LDA #11             ; print i
             STA $C7D5
-            LDA #7              ; print s
+            LDA #12             ; print s
             STA $C7D6
-            LDA #6              ; print i
+            LDA #11             ; print i
             STA $C7D7
-            LDA #2              ; print o
+            LDA #7              ; print o
             STA $C7D8
-            LDA #8              ; print n
-            STA $C7D9              
+            LDA #13             ; print n
+            STA $C7D9
+
+            LDA #14             ; tree 0
+            STA $C6A0
+            LDA #15             ; tree 1
+            STA $C6A1
+            LDA #16             ; tree 2
+            STA $C6A2
+            LDA #17             ; tree 3
+            STA $C6C9
+
+            LDA #$D5            ; set color of upper tree to light green (=D)
+            STA $DAA0
+            LDA #$D5
+            STA $DAA1
+            LDA #$D5
+            STA $DAA2
+            LDA #$95            ; set color of trunk to brown (=9)
+            STA $D6A9            
 
 _LOOP      
             LDA VIA_IORA        ; Read joystick
@@ -249,6 +278,51 @@ CHARDATA
   .BYTE %00000000
   .BYTE %00000000
 
+  .BYTE %00000000 ; house tile 0
+  .BYTE %00000001
+  .BYTE %01010101
+  .BYTE %10101010
+  .BYTE %10101000
+  .BYTE %10101000
+  .BYTE %10101010
+  .BYTE %00000000
+
+  .Byte %00010101 ; house tile 1
+  .Byte %01010101
+  .Byte %01010101
+  .Byte %10101010
+  .Byte %00101010
+  .Byte %00101010
+  .Byte %10101010
+  .Byte %00000000
+
+  .Byte %01010101 ; house tile 2
+  .Byte %01010101
+  .Byte %01010101
+  .Byte %10101010
+  .Byte %00001010
+  .Byte %00001010
+  .Byte %10101010
+  .Byte %00000000
+
+  .BYTE %01000000 ; house tile 3
+  .BYTE %01010100 
+  .BYTE %01010101 
+  .BYTE %10101010 
+  .BYTE %10000010 
+  .BYTE %10000010 
+  .BYTE %10101010 
+  .Byte %00000000
+
+  .Byte %00000000 ; house tile 4
+  .Byte %00000000
+  .Byte %01010000
+  .Byte %10100000
+  .Byte %10100000
+  .Byte %10100000
+  .Byte %10100000
+  .Byte %00000000
+
   .BYTE %00110000   ; C
   .BYTE %11001100
   .BYTE %11000000
@@ -320,6 +394,43 @@ CHARDATA
   .BYTE %11001100
   .BYTE %11001100
   .BYTE %11001100
+
+  .BYTE %00000000 ; tree 0
+  .BYTE %00000000
+  .BYTE %00000001
+  .BYTE %00000001
+  .BYTE %00000001
+  .BYTE %01010101
+  .BYTE %01010101
+  .BYTE %00000001
+
+  .BYTE %00010100 ; tree 1 
+  .BYTE %00010100 
+  .BYTE %01010101 
+  .BYTE %01010101 
+  .BYTE %01010101 
+  .BYTE %01010101 
+  .BYTE %01010101 
+  .BYTE %01010101 
+
+  .BYTE %00000000 ; tree 2
+  .BYTE %00000000
+  .BYTE %01000000
+  .BYTE %01000000
+  .BYTE %01000000
+  .BYTE %01010101
+  .BYTE %01010101
+  .BYTE %01000000
+
+  .BYTE %00101000 ; tree 4 / trunk (mind indentation)
+  .BYTE %00101000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+  .BYTE %00000000
+
 
 LAST                            ; End of the entire program
 
